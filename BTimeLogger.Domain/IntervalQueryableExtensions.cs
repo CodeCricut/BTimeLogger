ae62dec0538cd8070d5188da00350d918c5e8b8a
Andrew Richerson <ajorigami1@gmail.com>
2021-07-19 13:30:55 -0500

    Small changes.

---
+++ BTimeLogger.Domain/IntervalQueryableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace BTimeLogger.Domain
+{
+	public static class IntervalQueryableExtensions
+	{
+		public static IQueryable<Interval> BetweenDates(this IQueryable<Interval> intervals, DateTime from, DateTime to)
+		{
+			return intervals.Where(interval => interval.To >= from && interval.From <= to);
+		}
+	}
+}
--- BTimeLogger.Domain/Reporters/ActivityReporter.cs
+++ BTimeLogger.Domain/Reporters/ActivityReporter.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace BTimeLogger.Domain
 {
 	public interface IActivityReporter
 	{
-		ActivityReport Report(DateTime from, DateTime to);
+		Task<ActivityReport> Report(DateTime from, DateTime to);
 	}
 
 	public class ActivityReporter : IActivityReporter
 	{
-		public ActivityReport Report(DateTime from, DateTime to)
+		private readonly IStatisticsRepository _statisticsRepository;
+		private readonly IIntervalRepository _intervalRepository;
+
+		public ActivityReporter(
+			IStatisticsRepository statisticsRepository,
+			IIntervalRepository intervalRepository)
 		{
-			return new ActivityReport(); // TODO
+			_statisticsRepository = statisticsRepository;
+			_intervalRepository = intervalRepository;
+		}
+
+		public async Task<ActivityReport> Report(DateTime from, DateTime to)
+		{
+			//Statistic[] statistics = (await _statisticsRepository.GetStatistics()).ToArray();
+
+			//Interval[] intervals = (await _intervalRepository.GetIntervals())
+			//	.BetweenDates(from, to)
+			//	.ToArray();
+
+			//return new ActivityReport()
+			//{
+			//	From = from,
+			//	To = to,
+			//	Intervals = intervals,
+			//	Statistics = statistics
+			//};
+
+			return new ActivityReport();
 		}
 	}
 }
